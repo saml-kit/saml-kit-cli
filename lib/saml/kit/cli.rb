5ce53d5fd132c7589ee91dfb5c59cdba9f3836e4
mo <mo.khan@gmail.com>
2018-03-04 12:38:02 -0700

    fix broken specs.

--- lib/saml/kit/cli.rb
+++ lib/saml/kit/cli.rb
@@ -3,31 +3,28 @@ require 'thor'
 require 'yaml/store'
 require 'uri'
 
-require 'saml/kit/cli/certificate'
 require 'saml/kit/cli/certificate_report'
-require 'saml/kit/cli/decode'
-require 'saml/kit/cli/metadata'
 require 'saml/kit/cli/report'
 require 'saml/kit/cli/signature_report'
 require 'saml/kit/cli/version'
-require 'saml/kit/cli/xml_digital_signature'
 require 'saml/kit/cli/yaml_registry'
+require 'saml/kit/cli/commands'
 
 module Saml
   module Kit
     module Cli
       class Application < Thor
         desc 'decode SUBCOMMAND ...ARGS', 'decode SAMLRequest/SAMLResponse.'
-        subcommand 'decode', Decode
+        subcommand 'decode', Commands::Decode
 
         desc 'certificate SUBCOMMAND ...ARGS', 'Work with SAML Certificates.'
-        subcommand 'certificate', Certificate
+        subcommand 'certificate', Commands::Certificate
 
         desc 'metadata SUBCOMMAND ...ARGS', 'Work with SAML Metadata.'
-        subcommand 'metadata', Metadata
+        subcommand 'metadata', Commands::Metadata
 
         desc 'xmldsig SUBCOMMAND ...ARGS', 'Check XML digital signatures.'
-        subcommand 'xmldsig', XmlDigitalSignature
+        subcommand 'xmldsig', Commands::XmlDigitalSignature
       end
     end
   end
---
+++ lib/saml/kit/cli/commands.rb
@@ -0,0 +1,4 @@
+require 'saml/kit/cli/commands/certificate'
+require 'saml/kit/cli/commands/decode'
+require 'saml/kit/cli/commands/metadata'
+require 'saml/kit/cli/commands/xml_digital_signature'
