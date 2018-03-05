ce888449b832f0bbef1dfa87dfc44bc488c8f23b
mokha <mokha@cisco.com>
2018-03-05 16:47:36 -0700

    add specs for decoding a post.

--- spec/saml/kit/cli/commands/decode_spec.rb
+++ spec/saml/kit/cli/commands/decode_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe Saml::Kit::Cli::Commands::Certificate do
     specify { expect(output).to include("Decoded #{document.send(:name)}") }
     specify { pending; expect(output).to include(document.signature.certificate.x509.to_text) }
   end
+
+  describe "#psot" do
+    let(:command) { "decode post #{post_binding.serialize(builder)[1]['SAMLResponse']}" }
+    let(:document) { builder.build }
+    let(:user) { double(name_id_for: SecureRandom.uuid) }
+    let(:builder) do
+      Saml::Kit::Response.builder(user) do |x|
+        x.sign_with(Xml::Kit::KeyPair.generate(use: :signing))
+      end
+    end
+    let(:post_binding) do
+      Saml::Kit::Bindings::HttpPost.new(location: 'https://www.example.com/')
+    end
+
+    specify { expect(status).to be_success }
+    specify { expect(output).to include(document.to_xml(pretty: true)) }
+    specify { expect(output).to include("Decoded #{document.send(:name)}") }
+    specify { expect(output).to include(document.signature.certificate.x509.to_text) }
+  end
 end
