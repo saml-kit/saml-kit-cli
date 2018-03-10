e4c4152a4f011ad180e30e9a8b081b18db46e198
mokha <mokha@cisco.com>
2018-03-09 19:11:02 -0700

    fix broken test.

--- lib/saml/kit/core_ext/signature.rb
+++ lib/saml/kit/core_ext/signature.rb
@@ -5,7 +5,7 @@ module Saml
         'Digest Value' => ->(x) { x.digest_value },
         'Expected Digest Value' => ->(x) { x.expected_digest_value },
         'Digest Method' => ->(x) { x.digest_method },
-        'Signature Value' => ->(x) { x.truncate(signature_value) },
+        'Signature Value' => ->(x) { x.truncate(x.signature_value) },
         'Signature Method' => ->(x) { x.signature_method },
         'Canonicalization Method' => ->(x) { x.canonicalization_method },
       }.freeze
@@ -17,8 +17,6 @@ module Saml
         end
         table.push(['', certificate.x509.to_text])
       end
-
-      private
 
       def truncate(text, max: 50)
         text.length >= max ? "#{text[0..max]}..." : text
